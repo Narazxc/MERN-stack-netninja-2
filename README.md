3e2e035bedbdfceba0ff3047eb544cf550b9a653
Sitha Sovannara <sithasovannara51@gmail.com>
2023-07-24 14:07:17 +0700

    Created workout routes

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # MERN-Stack-Tutorial
+
 All course files for the MERN Stack Tutorial course on the Net Ninja YouTube channel &amp; the Net Ninja Pro website.
 
 ## How to use
+
 1. CD into the project directories (frontend AND backend)
 2. Run **npm install to install** all dependencies in **both** of those directories
 3. Run **npm start** to run the frontend application
 4. Run **npm run dev** to run the backend api
+
+## Workflow
+
+1. init app
+2. connect to db
+3. add in necessary middlewares (logger, json parser etc.)
+4. create router
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome to the app'})
-})
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+  console.log("listening on port", process.env.PORT);
+});
