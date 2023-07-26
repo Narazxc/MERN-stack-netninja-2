97a312863e436cc11f3efe8deb2ca856e918a0e3
Sitha Sovannara <sithasovannara51@gmail.com>
2023-07-26 10:11:00 +0700

    Frontend: Fetch data from the backend, created WorkoutDetails component and added some more css

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,16 @@
+export default function workoutDetails({ workout }) {
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
+
 export default function Home() {
+  const [workouts, setWorkouts] = useState(null);
+
+  // useEffect fire the function when the component is rendered
+  // second argument [] empty array (dependency array), implies that only fires the function once, when the component first rendered.
+  useEffect(() => {
+    // the purpose of creating a function inside use effect because we want to use async keyword
+    // we shouldn't use async directly in the callback function of useEffect
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 }
